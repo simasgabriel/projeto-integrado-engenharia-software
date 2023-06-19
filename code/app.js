08a3f0e95e01d1e6533545ff23e85613892ecc1f
simasgabriel <45041309+simasgabriel@users.noreply.github.com>
2023-06-19 14:44:27 -0300

    prototype using vue js

--- code/app.js
+++ code/app.js
@@ -7,6 +7,7 @@ mongoose.connect('mongodb://localhost:27017/restaurantesCriancas',{
     useUnifiedTopology: true
 });
 
+const Restaurante= require('./models/restaurante');
 const db=mongoose.connection;
 db.on('error', console.error.bind(console, "connection error"));
 db.once("open",() => {
@@ -23,6 +24,17 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/',(req,res)=>{
     res.render('home')
+})
+
+app.get('/testeRestaurante',(req,res)=>{
+    const rest = new Restaurante({ nomePerfilRestaurante :"Porcão",
+        endereco :"rua Leonel Magalhaes,68 -Charitas Niteroi Rj",
+        horarioFuncionamento:"10 h às 20h",
+        temFraldario :"Sim",
+        temBrinquedao:"Sim",
+    });
+    await rest.save();
+    res.send(rest)
 })
 app.listen(3000,()=>{
     console.log('serving on port 3000');
