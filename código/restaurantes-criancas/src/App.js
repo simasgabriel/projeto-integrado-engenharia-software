f69fcfa755e706ace681aa3fc198ff03ad1ebe12
simasgabriel <45041309+simasgabriel@users.noreply.github.com>
2023-07-07 19:54:50 -0300

    navbar for users
    mudar navbar para usuarios autenticados ou nao

--- código/restaurantes-criancas/src/App.js
+++ código/restaurantes-criancas/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
 import Home from "./pages/Home/Home";
 import QuemSomos from "./pages/QuemSomos/QuemSomos";
 import Navbar from "./components/Navbar";
@@ -10,23 +11,46 @@ import TituloMain from "./components/TituloMain";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
 
+// context
+import { AuthProvider } from "./context/AuthContext";
+
+// hooks
+import { useState, useEffect } from "react";
+import { useAuthentication } from "./hooks/useAuthentication";
+
 function App() {
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
+  const loadingUser = user === undefined;
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, [auth]);
+
+  if (loadingUser) {
+    return <p>Carregando...</p>;
+  }
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <TituloMain />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/quemsomos" element={<QuemSomos />} />
-            <Route path="/faleconosco" element={<FaleConosco />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider value={{ user }}>
+        <BrowserRouter>
+          <Navbar />
+          <TituloMain />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/quemsomos" element={<QuemSomos />} />
+              <Route path="/faleconosco" element={<FaleConosco />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
--- código/restaurantes-criancas/src/components/Navbar.js
+++ código/restaurantes-criancas/src/components/Navbar.js
@@ -1,21 +1,63 @@
 import { NavLink } from "react-router-dom";
 import styles from "./Navbar.module.css";
+import { useAuthentication } from "../hooks/useAuthentication";
+import { useAuthValue } from "../context/AuthContext";
 
 const Navbar = () => {
+  const { user } = useAuthValue();
   return (
     <nav className={styles.navbar}>
       <NavLink to="/" className={styles.brand}>
         Restaurantes para Crianças
       </NavLink>
 
-      <ul className={styles.links_list}>
-        <li>
-          <NavLink to="/Register">Cadastrar-se</NavLink>
+      {!user && (
+        <>
+          <li className={styles.links_list}>
+            <NavLink
+              to="/login"
+              className={({ isActive }) => (isActive ? styles.active : "")}
+            >
+              Entrar
+            </NavLink>
+          </li>
+          <li className={styles.links_list}>
+            <NavLink
+              to="/register"
+              className={({ isActive }) => (isActive ? styles.active : "")}
+            >
+              Cadastrar-se
+            </NavLink>
+          </li>
+        </>
+      )}
+      {user && (
+        <>
+          <li className={styles.links_list}>
+            <NavLink
+              to="/posts/create"
+              className={({ isActive }) => (isActive ? styles.active : "")}
+            >
+              Criar novo perfil de restaurante
+            </NavLink>
+          </li>
+          <li className={styles.links_list}>
+            <NavLink
+              to="/dashboard"
+              className={({ isActive }) => (isActive ? styles.active : "")}
+            >
+              Meus posts
+            </NavLink>
+          </li>
+        </>
+      )}
+      {user && (
+        <li className={styles.links_list}>
+          <button className={styles.btn} onClick>
+            Sair
+          </button>
         </li>
-        <li>
-          <NavLink to="/Login">Entrar </NavLink>
-        </li>
-      </ul>
+      )}
     </nav>
   );
 };
