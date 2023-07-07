b84e78878db93455c8472ad0f9dd0487ef7a673c
simasgabriel <45041309+simasgabriel@users.noreply.github.com>
2023-07-07 15:11:53 -0300

    tituloMain
    styles

--- código/restaurantes-criancas/src/components/TituloMain.js
+++ código/restaurantes-criancas/src/components/TituloMain.js
@@ -2,7 +2,15 @@ import styles from "./TituloMain.module.css";
 import { NavLink } from "react-router-dom";
 
 const TituloMain = () => {
-  return <div className={styles.titulo}></div>;
+  return (
+    <div className={styles.titulo}>
+      <div className={styles.brand}>Restaurantes para Crianças</div>
+      <p>
+        Descubra e avalie estabelecimentos com espaço kids (ou brinquedão),
+        fraldário e cadeirão
+      </p>
+    </div>
+  );
 };
 
 export default TituloMain;
