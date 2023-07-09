be5b66895e3b136a4b34e066a2b8857e998fa0c4
simasgabriel <45041309+simasgabriel@users.noreply.github.com>
2023-07-09 17:23:46 -0300

    hook fetch documents

--- código/restaurantes-criancas/src/pages/Home/Home.js
+++ código/restaurantes-criancas/src/pages/Home/Home.js
@@ -1,7 +1,56 @@
+
 import styles from "./Home.module.css";
 
+// hooks
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+
+// components
+import PostDetail from "../../components/PostDetail";
+
 const Home = () => {
-  return <div>Home</div>;
+  //const { documents: posts, loading } = useFetchDocuments("posts");
+
+  const navigate = useNavigate();
+
+  const [query, setQuery] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (query) {
+      return navigate(`/search?q=${query}`);
+    }
+  };
+
+  console.log(loading);
+
+  return (
+    <div className={styles.home}>
+      <h1>Veja os últimos perfis de restaurantes publicados</h1>
+      <form className={styles.search_form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Ou busque por nome do restaurante, bairro ou cidade..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn ">Pesquisar</button>
+      </form>
+      <div className="post-list">
+        {loading && <p>Carregando...</p>}
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create" className="btn">
+              Criar primeiro post
+            </Link>
+          </div>
+        )}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
