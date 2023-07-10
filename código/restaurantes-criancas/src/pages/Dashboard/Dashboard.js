c080ffed31f70b591463089f392cfed524a18a30
simasgabriel <45041309+simasgabriel@users.noreply.github.com>
2023-07-10 12:04:31 -0300

    seção meus posts

--- código/restaurantes-criancas/src/pages/Dashboard/Dashboard.js
+++ código/restaurantes-criancas/src/pages/Dashboard/Dashboard.js
@@ -1,14 +1,62 @@
 import styles from "./Dashboard.module.css";
 
+import { Link } from "react-router-dom";
 
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+import { useDeleteDocument } from "../../hooks/useDeleteDocument";
 
 const Dashboard = () => {
+  const { user } = useAuthValue();
+  const uid = user.uid;
+
+  const { documents: posts } = useFetchDocuments("posts", null, uid);
+
+  const { deleteDocument } = useDeleteDocument("posts");
+
+  console.log(uid);
+  console.log(posts);
+
   return (
-    <div>Dashboard
+    <div className={styles.dashboard}>
+      <h2>Meus posts</h2>
+      <p>Gerencie os perfis de restaurante criados por você</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Não foram encontrados posts</p>
+          <Link to="/posts/create" className="btn">
+            Criar primeiro post
+          </Link>
+        </div>
+      ) : (
+        <div className={styles.post_header}>
+          <span>Título</span>
+          <span>Ações</span>
+        </div>
+      )}
 
-      <h1>Dashboard</h1>
+      {posts &&
+        posts.map((post) => (
+          <div className={styles.post_row} key={post.id}>
+            <p>{post.title}</p>
+            <div className={styles.actions}>
+              <Link to={`/posts/${post.id}`} className="btn ">
+                Ver
+              </Link>
+              <Link to={`/posts/edit/${post.id}`} className="btn ">
+                Editar
+              </Link>
+              <button
+                onClick={() => deleteDocument(post.id)}
+                className="btn btn-danger"
+              >
+                Excluir
+              </button>
+            </div>
+          </div>
+        ))}
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
